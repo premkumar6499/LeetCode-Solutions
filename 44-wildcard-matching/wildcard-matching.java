06961fa997091be61e6f9dfa5623ac02087e955d
premkumar6499 <premjiguru_mca27@mepcoeng.ac.in>
2026-03-02 21:13:33 +0530

    Time: 54 ms (24.97%) | Memory: 47.7 MB (42.55%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        // Base case: empty string matches empty pattern
+        dp[0][0] = true;
+
+        // Base case: handle patterns starting with '*'
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '?' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    // Current characters match
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    // '*' matches empty (dp[i][j-1]) or at least one char (dp[i-1][j])
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
